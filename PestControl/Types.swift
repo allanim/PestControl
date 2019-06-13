a44e38bf09a1bfe61224f4462543117f6663a3da
Allan Im <allan@vogle.com>
2019-06-13 19:38:01 -0400

    animation

---
+++ PestControl/Types.swift
@@ -0,0 +1,11 @@
+//
+//  Types.swift
+//  PestControl
+//
+//  Created by Allan Im on 2019-06-13.
+//  Copyright © 2019 Ray Wenderlich. All rights reserved.
+//
+
+enum Direction: Int {
+  case forward = 0, backward, left, right
+}
